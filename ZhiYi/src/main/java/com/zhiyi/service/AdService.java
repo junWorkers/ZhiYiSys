5a0d980913f5fc76b9a36a8a639c34564c2a493e
wy86980510 <86980510@qq.com>
2016-05-30 21:23:03 +0800

    广告位

--- ZhiYi/src/main/java/com/zhiyi/service/AdService.java
+++ ZhiYi/src/main/java/com/zhiyi/service/AdService.java
@@ -2,8 +2,15 @@ package com.zhiyi.service;
 
 import java.util.List;
 
+import com.zhiyi.beans.JsonObject;
 import com.zhiyi.entity.Ad;
 
 public interface AdService {
 	List<Ad> shoufindpic(); //首页大图
+
+	JsonObject<Ad> getPageAdInfo(String page, String rows);
+
+	int getTotal();
+
+	JsonObject<Ad> getPageAll(String page, String rows);
 }
--- ZhiYi/src/main/java/com/zhiyi/service/impl/AdServiceImpl.java
+++ ZhiYi/src/main/java/com/zhiyi/service/impl/AdServiceImpl.java
@@ -1,11 +1,15 @@
 package com.zhiyi.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zhiyi.beans.JsonObject;
 import com.zhiyi.entity.Ad;
+import com.zhiyi.entity.Admin;
 import com.zhiyi.mapper.AdMapper;
 import com.zhiyi.service.AdService;
 @Service("adService")
@@ -15,6 +19,36 @@ public class AdServiceImpl implements AdService {
 	@Override
 	public List<Ad> shoufindpic() {
 		return adMapper.showfndpic();
+	}
+	@Override
+	public JsonObject<Ad> getPageAdInfo(String page, String rows) {
+		JsonObject<Ad> jsonObject = new JsonObject<Ad>();
+		jsonObject.setTotal(getTotal());
+		jsonObject.setRows(find(page, rows));
+		return jsonObject;
+	}
+	private List<Ad> find(String page, String rows) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("min", (Integer.parseInt(page) - 1) * Integer.parseInt(rows));
+		params.put("max", Integer.parseInt(page) * Integer.parseInt(rows));
+		return adMapper.find(params);
+	}
+	@Override
+	public int getTotal() {
+		return adMapper.getTotal();
+	}
+	@Override
+	public JsonObject<Ad> getPageAll(String page, String rows) {
+		JsonObject<Ad> jsonObject = new JsonObject<Ad>();
+		jsonObject.setTotal(getTotal());
+		jsonObject.setRows(finds(page, rows));
+		return jsonObject;
+	}
+	private List<Ad> finds(String page, String rows) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("min", (Integer.parseInt(page) - 1) * Integer.parseInt(rows));
+		params.put("max", Integer.parseInt(page) * Integer.parseInt(rows));
+		return adMapper.finds(params);
 	}
 
 }
